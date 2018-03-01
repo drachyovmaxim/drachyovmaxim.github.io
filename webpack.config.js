68edf85024205dba644788296f67c4f346ca39d4
Maxim Drachev <drachyov.maxim@gmail.com>
2018-03-02 02:08:16 +0300

    update

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/js/main.js',
+
+  output: {
+    path: __dirname,
+    filename: 'js/app.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('css/app.css', { allChunks: true }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        }),
+        include: __dirname
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: 'file?name=src/fonts/[name].[ext]'
+      },
+    ]
+  },
+
+  devServer: {
+    historyApiFallback: true
+  },
+
+  stats: {
+    colors: true
+  }
+};
